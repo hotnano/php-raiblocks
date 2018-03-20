840b13243bd2f6808c4df51a9fa4827bd32832e4
Mr. Nano <nanocoinio@gmail.com>
2018-03-20 21:26:45 +0100

    Check source and destination before sending any Nanos.

--- src/Action/Sending.php
+++ src/Action/Sending.php
@@ -6,6 +6,13 @@ trait Sending
 {
     public function send(string $wallet, string $source, string $destination, string $amount, ?string $id = null)
     {
+        if (!$source) {
+            throw new \RuntimeException('Invalid source.');
+        }
+        if (!$destination) {
+            throw new \RuntimeException('Invalid destination.');
+        }
+
         $params = [
             'action' => 'send',
             'wallet' => $wallet,
